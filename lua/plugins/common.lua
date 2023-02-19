6543c16470db4ac63bc9182515fc3f55243d0401
Matej Janežič <janezic.mj@gmail.com>
2023-02-19 23:18:06 +0100

    feat: add neo-tree.nvim

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -105,6 +105,22 @@ return {
     end,
   },
 
+  { -- file tree
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function ()
+      -- Unless you are still migrating, remove the deprecated commands from v1.x
+      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+      require('neo-tree').setup {}
+    end,
+  },
+
   -- NOTE: Finally, themes
   -- FIX: figure out how to switch between themes more easily
 
