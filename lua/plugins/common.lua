e9a413c15c2477d912cbebfd57c1445a6b6875af
Matej Janežič <janezic.mj@gmail.com>
2023-03-21 16:00:33 +0100

    common: add luatree

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -135,7 +135,15 @@ return {
       -- Unless you are still migrating, remove the deprecated commands from v1.x
       vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-      require('neo-tree').setup {}
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        }
+      }
     end,
   },
 }
