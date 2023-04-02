43fdd586fd23a1b2f722a66c1d27073a1d831fd6
Matej Janežič <janezic.mj@gmail.com>
2023-04-02 23:35:30 +0200

    common: change theme settings

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -5,8 +5,8 @@ return {
     priority = 1000,
     config = function()
       vim.g.gruvbox_material_background = 'soft'
-      vim.g.gruvbox_material_foreground = 'original'
       vim.g.gruvbox_material_better_performance = 1
+      -- vim.g.gruvbox_material_enable_italic = 1
 
       vim.cmd.colorscheme 'gruvbox-material'
     end,
