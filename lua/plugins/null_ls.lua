85fcc33ba6bf9f17cc4dcc57556d251630e25d0c
Matej Janežič <janezic.mj@gmail.com>
2023-05-19 19:42:52 +0200

    chore: decouple null-ls from lsp

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,23 @@
+return {
+  -- inject lsp formatting, diagonstics etc
+  'jose-elias-alvarez/null-ls.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local null_ls = require 'null-ls'
+
+    null_ls.setup {
+      sources = {
+        -- general
+        null_ls.builtins.code_actions.gitsigns,
+        -- python
+        -- null_ls.builtins.diagnostics.pylint,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.isort,
+        -- typescript
+        null_ls.builtins.formatting.prettier,
+        -- lua
+        null_ls.builtins.formatting.stylua,
+      },
+    }
+  end,
+}
