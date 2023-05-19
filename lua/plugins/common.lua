be676d0b07fd70fbf131cd83453ab190bb5855b1
Matej Janežič <janezic.mj@gmail.com>
2023-05-20 00:57:59 +0200

    chore: decouple wilder from common

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -129,41 +129,6 @@ return {
   },
 
   {
-    -- a more adventurous wildmenu
-    'gelguy/wilder.nvim',
-    dependencies = { 'romgrk/fzy-lua-native' },
-    config = function()
-      local wilder = require 'wilder'
-      wilder.setup { modes = { ':', '/', '?' } }
-      -- Disable Python remote plugin
-      wilder.set_option('use_python_remote_plugin', 0)
-
-      wilder.set_option('pipeline', {
-        wilder.branch(
-          wilder.cmdline_pipeline {
-            fuzzy = 1,
-            fuzzy_filter = wilder.lua_fzy_filter(),
-          },
-          wilder.vim_search_pipeline()
-        ),
-      })
-
-      wilder.set_option(
-        'renderer',
-        wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
-          highlights = {
-            border = 'Normal',
-          },
-          border = 'rounded',
-          highlighter = wilder.lua_fzy_highlighter(),
-          left = { ' ', wilder.popupmenu_devicons() },
-          right = { ' ', wilder.popupmenu_scrollbar() },
-        })
-      )
-    end,
-  },
-
-  {
     'projekt0n/circles.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
