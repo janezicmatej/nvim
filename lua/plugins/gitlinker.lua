6099693109b9e948ac890bd4f4304eec20427ec8
Matej Janežič <janezic.mj@gmail.com>
2023-06-16 17:24:34 +0200

    feat(gitlinker): add git.janezic.dev gitea

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -4,6 +4,7 @@ return {
     require('gitlinker').setup {
       callbacks = {
         ['git.aflabs.org'] = require('gitlinker.hosts').get_gitlab_type_url,
+        ['git.janezic.dev'] = require('gitlinker.hosts').get_gitea_type_url,
       },
     }
   end,
