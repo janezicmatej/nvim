ba3788094023f67f66ee10017a47e122a3fa3189
Matej Janežič <janezic.mj@gmail.com>
2023-06-16 17:25:34 +0200

    feat(oil): show hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    require('oil').setup { view_options = { show_hidden = true } }
     vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
   end,
 }
