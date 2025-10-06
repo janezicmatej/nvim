8c6fda9abb077f5a855cc01b150fad8266013e62
Matej Janežič <janezic.mj@gmail.com>
2025-10-07 00:45:12 +0200

    chore: remove uneeded comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,9 +11,5 @@ now(function()
     vim.g.gruvbox_material_enable_bold = 1
     vim.g.gruvbox_material_enable_italic = 1
 
-    -- vim.api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
-    -- vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
-
     vim.cmd.colorscheme("gruvbox-material")
 end)
