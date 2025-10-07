9c1293799273eb3bb07fa71e7aeaebd1b39bd072
Matej Janežič <janezic.mj@gmail.com>
2025-10-08 01:40:35 +0200

    feat: git_blame disabled on start

--- lua/plugins/git_blame.lua
+++ lua/plugins/git_blame.lua
@@ -1,7 +1,7 @@
 MiniDeps.later(function()
     MiniDeps.add("f-person/git-blame.nvim")
     require("gitblame").setup({
-        enabled = true,
+        enabled = false,
         message_template = "  <sha>: <author> <date> <summary>",
         date_format = "%Y-%m-%d",
     })
