279be9ce33bf3457513a79c1c182a3ce5affed2e
Matej Janežič <janezic.mj@gmail.com>
2025-10-08 01:37:02 +0200

    feat: add after/ftplugin for lua, python, rust

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
+
+vim.opt_local.colorcolumn = { 81 }
