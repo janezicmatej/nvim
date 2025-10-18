513f2e3e201a206bb61609e5cee87304753b44dd
Matej Janežič <janezic.mj@gmail.com>
2025-10-18 02:56:49 +0200

    feat: use vim.tbl_contains api where possible

--- lua/plugins/mini_indentscope.lua
+++ lua/plugins/mini_indentscope.lua
@@ -12,15 +12,10 @@ MiniDeps.later(function()
         "qf",
     }
 
-    local disable_set = {}
-    for _, v in ipairs(disable) do
-        disable_set[v] = true
-    end
-
     local function term_disable(args) vim.b[args.buf].miniindentscope_disable = true end
     local function ft_disable(args)
         local ft = vim.bo[args.buf].filetype
-        if disable_set[ft] == nil then return end
+        if not vim.tbl_contains(disable, ft) then return end
         vim.b[args.buf].miniindentscope_disable = true
     end
 
