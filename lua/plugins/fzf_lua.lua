df31db15d938838a5f4a0a699e3bb92e02b4cb64
Matej Janežič <janezic.mj@gmail.com>
2025-10-25 14:03:18 +0200

    feat: add helptags and jumps fzf keymaps

--- lua/plugins/fzf_lua.lua
+++ lua/plugins/fzf_lua.lua
@@ -1,8 +1,8 @@
 MiniDeps.later(function()
     MiniDeps.add("ibhagwan/fzf-lua")
 
-    local fzf = require("fzf-lua")
-    fzf.setup({
+    local fzf_lua = require("fzf-lua")
+    fzf_lua.setup({
         files = {
             no_ignore = true,
             toggle_ignore_flag = "--no-ignore-vcs",
@@ -13,11 +13,12 @@ MiniDeps.later(function()
         },
     })
 
-    vim.keymap.set("n", "<leader>sf", fzf.files)
-    vim.keymap.set("n", "<leader>sg", fzf.live_grep)
-    vim.keymap.set("n", "<leader>sw", fzf.grep_cword)
-    vim.keymap.set("v", "<leader>sg", fzf.grep_visual)
-
-    vim.keymap.set("n", "<leader>sr", fzf.resume)
-    vim.keymap.set("n", "<leader>sb", fzf.builtin)
+    vim.keymap.set("n", "<leader>sf", fzf_lua.files)
+    vim.keymap.set("n", "<leader>sg", fzf_lua.live_grep)
+    vim.keymap.set("n", "<leader>sw", fzf_lua.grep_cword)
+    vim.keymap.set("v", "<leader>sg", fzf_lua.grep_visual)
+    vim.keymap.set("n", "<leader>sj", fzf_lua.jumps)
+    vim.keymap.set("n", "<leader>sh", fzf_lua.helptags)
+    vim.keymap.set("n", "<leader>sr", fzf_lua.resume)
+    vim.keymap.set("n", "<leader>sb", fzf_lua.builtin)
 end)
