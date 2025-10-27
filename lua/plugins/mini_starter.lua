e52cd237a3a117601803bcebe9667e446dc03233
Matej Janežič <janezic.mj@gmail.com>
2025-10-27 18:26:04 +0100

    feat: add count of dependencies and updating from stdpath("config")

--- lua/plugins/mini_starter.lua
+++ lua/plugins/mini_starter.lua
@@ -12,25 +12,60 @@ local function reinstall_treesitter()
     ts.install(ts.get_installed(), { force = true })
 end
 
+local function deps_section()
+    local mini_deps = require("mini.deps")
+    local nr_deps = #vim.iter(mini_deps):enumerate():totable()
+
+    local items = {
+        {
+            name = "number of dependencies " .. nr_deps,
+            action = "",
+            section = "dependencies",
+        },
+    }
+
+    local function mk_dep_item(name, action)
+        return { name = name, action = action, section = "dependencies" }
+    end
+
+    local deps_actions = {
+        mk_dep_item("update dependencies", "DepsUpdate"),
+        mk_dep_item("snap dependencies", "DepsSnapSave"),
+        mk_dep_item("load dependencies", "DepsSnapLoad"),
+        mk_dep_item("clean dependencies", "DepsClean"),
+    }
+
+    if vim.fn.getcwd() == vim.fn.stdpath("config") then
+        for _, item in ipairs(deps_actions) do
+            table.insert(items, item)
+        end
+    end
+
+    return items
+end
+
 MiniDeps.now(function()
+    local builtins = {
+        { name = "edit new buffer", action = "enew", section = "builtin actions" },
+        { name = "quit neovim", action = "qall", section = "builtin actions" },
+    }
+
     require("mini.starter").setup({
         query_updaters = "abcdefghijklmnopqrstuvwxyz0123456789_.",
         evaluate_single = true,
         header = default_header,
         footer = "",
         items = {
-            --stylua: ignore start
             -- builtins
-            { name = "edit new buffer",              action = "enew",               section = "builtin actions" },
-            { name = "quit neovim",                  action = "qall",               section = "builtin actions" },
+            builtins,
             -- dependencies
-            { name = "update dependencies",          action = "DepsUpdate",         section = "dependencies" },
-            { name = "snap dependencies",            action = "DepsSnapSave",       section = "dependencies" },
-            { name = "load dependencies",            action = "DepsSnapLoad",       section = "dependencies" },
-            { name = "clean dependencies",           action = "DepsClean",          section = "dependencies" },
+            deps_section,
             -- debug
-            { name = "reinstall treesitter parsers", action = reinstall_treesitter, section = "debug" },
-            --stylua: ignore end
+            {
+                name = "reinstall treesitter parsers",
+                action = reinstall_treesitter,
+                section = "debug",
+            },
         },
     })
 end)
