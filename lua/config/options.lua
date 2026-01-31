5b7104b7d3cb4319e6558b7735152c7603b13ab2
Matej Janežič <janezic.mj@gmail.com>
2026-01-31 01:30:02 +0100

    feat: set swayconfig ft for *.conf subfiles of ~/.config/sway

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -68,3 +68,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("CustomHighlightYank", { clear = true }),
     callback = function() vim.highlight.on_yank() end,
 })
+
+-- filetype overrides
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = vim.fn.expand("~") .. "/.config/sway/**/*.conf",
+    callback = function() vim.bo.filetype = "swayconfig" end,
+})
