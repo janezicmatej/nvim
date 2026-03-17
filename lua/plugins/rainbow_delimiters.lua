e26e6517fdd2186f3ffc55f42cee8566346a936d
Matej Janežič <janezic.mj@gmail.com>
2026-03-17 22:34:14 +0100

    fix: temporarily wrap rainbow-delimiters

--- lua/plugins/rainbow_delimiters.lua
+++ lua/plugins/rainbow_delimiters.lua
@@ -1,4 +1,14 @@
 MiniDeps.later(function()
     MiniDeps.add("HiPhish/rainbow-delimiters.nvim")
+
+    -- HACK:(@janezicmatej) neovim 0.12 get_parser returns nil instead of erroring
+    local lib = require("rainbow-delimiters.lib")
+    local original_attach = lib.attach
+    lib.attach = function(bufnr, lang)
+        local parser = vim.treesitter.get_parser(bufnr, lang, { error = false })
+        if not parser then return end
+        return original_attach(bufnr, lang)
+    end
+
     require("rainbow-delimiters.setup").setup()
 end)
